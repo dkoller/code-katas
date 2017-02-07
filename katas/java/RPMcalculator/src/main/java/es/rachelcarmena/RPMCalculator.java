f99b8e2ed4df599edc65272bfec4b5d8236af82b
Rachel <raquel.moreno.carmena@gmail.com>
2017-02-07 22:02:06 +0100

    Refactoring

--- katas/java/RPMcalculator/src/main/java/es/rachelcarmena/RPMCalculator.java
+++ katas/java/RPMcalculator/src/main/java/es/rachelcarmena/RPMCalculator.java
@@ -12,8 +12,12 @@ public class RPMCalculator {
 		return Pattern.matches("\\d+(\\s\\d+)*", expression);
 	}
 
-	private String[] getComponents(String expression) {
-		return expression.split("\\s");
+	private boolean areDigits(List<String> components) {
+		return (components.stream().allMatch(e -> areDigits(e)));
+	}
+
+	private List<String> getComponents(String expression) {
+		return Arrays.asList(expression.split("\\s"));
 	}
 
 	private boolean isOperator(String component) {
@@ -34,7 +38,7 @@ public class RPMCalculator {
 		return 0;
 	}
 
-	private int getIndexOfFirstOperator(String[] components) {
+	private int getIndexOfFirstOperator(List<String> components) {
 		int index = 0;
 		for (String component : components) {
 			if (isOperator(component))
@@ -44,34 +48,39 @@ public class RPMCalculator {
 		return index;
 	}
 
-	private String operateToIndexOfOperator(String[] components, int indexOfOperator) {
-		char operator = components[indexOfOperator].charAt(0);
-		int rightOperand = Integer.valueOf(components[indexOfOperator - 1]);
-		int leftOperand = Integer.valueOf(components[indexOfOperator - 2]);
+	private String operateToIndexOfOperator(List<String> components, int indexOfOperator) {
+		char operator = components.get(indexOfOperator).charAt(0);
+		int rightOperand = Integer.valueOf(components.get(indexOfOperator - 1));
+		int leftOperand = Integer.valueOf(components.get(indexOfOperator - 2));
 
 		return String.valueOf(operate(leftOperand, rightOperand, operator));
 	}
 
-	private String getNewExpression(String[] components, int indexOfOperator, String partialResult) {
-		List<String> newListOfComponents = new ArrayList<String>(Arrays.asList(components));
+	private List<String> reduceComponents(List<String> components, int indexOfOperator, String partialResult) {
+		List<String> newListOfComponents = new ArrayList<String>(components);
 		newListOfComponents.subList(indexOfOperator - 2, indexOfOperator + 1).clear();
 		newListOfComponents.add(indexOfOperator - 2, partialResult);
-		return newListOfComponents.stream().collect(Collectors.joining(" "));
+		return newListOfComponents;
+	}
+
+	private List<String> evaluateComponents(List<String> components) {
+		int indexOfFirstOperator = getIndexOfFirstOperator(components);
+		if (indexOfFirstOperator < 2)
+			throw new IllegalArgumentException();
+
+		String partialResult = operateToIndexOfOperator(components, indexOfFirstOperator);
+		List<String> newComponents = reduceComponents(components, indexOfFirstOperator, partialResult);
+		if (areDigits(newComponents))
+			return newComponents;
+
+		return evaluateComponents(newComponents);
 	}
 
 	public String evaluate(String expression) throws IllegalArgumentException {
 		if (areDigits(expression))
 			return expression;
 
-		String[] components = getComponents(expression);
-		int indexOfFirstOperator = getIndexOfFirstOperator(components);
-		if (indexOfFirstOperator < 2)
-			throw new IllegalArgumentException();
-
-		String partialResult = operateToIndexOfOperator(components, indexOfFirstOperator);
-
-		if (components.length == 3)
-			return partialResult;
-		return evaluate(getNewExpression(components, indexOfFirstOperator, partialResult));
+		List<String> components = getComponents(expression);
+		return evaluateComponents(components).stream().collect(Collectors.joining(" "));
 	}
 }
