50d4f71e010771f04b8482ae71f26e7b36e5f7cb
Rachel <raquel.moreno.carmena@gmail.com>
2017-02-07 18:58:50 +0100

    Refactoring

--- katas/java/diamond/src/main/java/es/rachelcarmena/ListUtils.java
+++ katas/java/diamond/src/main/java/es/rachelcarmena/ListUtils.java
@@ -6,10 +6,7 @@ import java.util.List;
 public class ListUtils {
 	
 	public static List<String> addReverse(List<String> initialList) {
-		List<String> newList = new ArrayList<>();
-		for (String elem : initialList) {
-			newList.add(elem);
-		}
+		List<String> newList = new ArrayList<>(initialList);
 		for (int i = initialList.size() - 2; i >= 0; i--) {
 			newList.add(initialList.get(i));
 		}
