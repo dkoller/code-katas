b570e383f3d7ee0b6bb52a8ecdfdd2c08ba7f2cd
Rachel <raquel.moreno.carmena@gmail.com>
2017-02-11 10:53:44 +0100

    Refactoring

--- katas/java/virtual-disk/src/main/java/es/rachelcarmena/operation/MP3Counter.java
+++ katas/java/virtual-disk/src/main/java/es/rachelcarmena/operation/MP3Counter.java
@@ -1,10 +1,8 @@
 package es.rachelcarmena.operation;
 
 import es.rachelcarmena.element.SContainer;
-import es.rachelcarmena.element.SDirectory;
 import es.rachelcarmena.element.SResource;
 import es.rachelcarmena.element.SStandardFile;
-import es.rachelcarmena.element.SZipFile;
 
 public class MP3Counter implements Visitor {
 
@@ -25,12 +23,7 @@ public class MP3Counter implements Visitor {
 
 	public void visit(SContainer container) {
 		for (SResource resource : container.getResources()) {
-			if (resource instanceof SDirectory)
-				visit((SDirectory) resource);
-			else if (resource instanceof SStandardFile)
-				visit((SStandardFile) resource);
-			else if (resource instanceof SZipFile)
-				visit((SZipFile) resource);
+			resource.accept(this);
 		}
 	}
 }
