74501d364281abaf4a8abca2c9a162a1e217e3cf
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 17:38:42 +0100

    Refactoring: removing duplication

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
@@ -1,6 +1,8 @@
 package es.rachelcarmena;
 
 public class SalarySlipGenerator {
+    private static final String HEADER_EMPLOYEE_ID = "Employee ID";
+    private static final String HEADER_EMPLOYEE_NAME = "Employee Name";
     private SalarySlipPrinter salarySlipPrinter;
 
     public SalarySlipGenerator(SalarySlipPrinter salarySlipPrinter) {
@@ -8,7 +10,22 @@ public class SalarySlipGenerator {
     }
 
     public void generateFor(Employee employee) {
-        salarySlipPrinter.printLine("Employee ID: " + String.valueOf(employee.employeeID));
-        salarySlipPrinter.printLine("Employee Name: " + String.valueOf(employee.employeeName));
+        String line = createLineForEmployeeID(employee.employeeID);
+        salarySlipPrinter.printLine(line);
+
+        line = createLineForEmployeeName(employee.employeeName);
+        salarySlipPrinter.printLine(line);
+    }
+
+    private String createLineForEmployeeID(int employeeID) {
+        return formatPrintedLine(HEADER_EMPLOYEE_ID, String.valueOf(employeeID));
+    }
+
+    private String createLineForEmployeeName(String employeeName) {
+        return formatPrintedLine(HEADER_EMPLOYEE_NAME, employeeName);
+    }
+
+    private String formatPrintedLine(String header, String value) {
+        return String.format("%s: %s", header, value);
     }
 }
