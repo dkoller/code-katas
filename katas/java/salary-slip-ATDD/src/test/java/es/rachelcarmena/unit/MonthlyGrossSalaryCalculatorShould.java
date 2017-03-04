305fcacf5d89787e7963c8326c9110306ab29cd5
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 19:06:43 +0100

    Red unit test

--- katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/MonthlyGrossSalaryCalculatorShould.java
+++ katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/MonthlyGrossSalaryCalculatorShould.java
@@ -11,10 +11,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class MonthlyGrossSalaryCalculatorShould {
 
     @Test
-    public void calculate_monthly_gross_salary_for_gross_annual_salary() {
+    public void calculate_monthly_gross_salary_for_gross_annual_salary_24000() {
         MonthlyGrossSalaryCalculator monthlyGrossSalaryCalculator = new MonthlyGrossSalaryCalculator();
         AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(24000);
         MonthlyGrossSalary monthlyGrossSalary = new MonthlyGrossSalary(2000);
+        assertThat(monthlyGrossSalaryCalculator.calculate(annualGrossSalary), is(monthlyGrossSalary));
+    }
+
+    @Test
+    public void calculate_monthly_gross_salary_for_gross_annual_salary_49200() {
+        MonthlyGrossSalaryCalculator monthlyGrossSalaryCalculator = new MonthlyGrossSalaryCalculator();
+        AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(49200);
+        MonthlyGrossSalary monthlyGrossSalary = new MonthlyGrossSalary(4100);
         assertThat(monthlyGrossSalaryCalculator.calculate(annualGrossSalary), is(monthlyGrossSalary));
     }
 }
