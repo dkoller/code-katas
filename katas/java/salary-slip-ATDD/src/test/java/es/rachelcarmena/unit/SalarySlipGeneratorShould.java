26053929d276043ac0d3eee8b1da2d883acce13a
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 17:47:51 +0100

    Refactoring: removing unnecessary unit tests

--- katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/SalarySlipGeneratorShould.java
+++ katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/SalarySlipGeneratorShould.java
@@ -18,8 +18,7 @@ public class SalarySlipGeneratorShould {
     @Mock SalarySlipPrinter salarySlipPrinter;
 
     private static final AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(24000);
-    private static final Employee EMPLOYEE_12345_JOHN = new Employee(12345, "John J Doe", annualGrossSalary);
-    private static final Employee EMPLOYEE_56789_PAUL = new Employee(56789, "Paul J Doe", annualGrossSalary);
+    private static final Employee EMPLOYEE = new Employee(12345, "John J Doe", annualGrossSalary);
 
     private SalarySlipGenerator salarySlipGenerator;
 
@@ -29,30 +28,16 @@ public class SalarySlipGeneratorShould {
     }
 
     @Test
-    public void print_employeID_12345_for_an_employee_with_ID_12345() {
-        salarySlipGenerator.generateFor(EMPLOYEE_12345_JOHN);
+    public void print_employeID_for_an_employee() {
+        salarySlipGenerator.generateFor(EMPLOYEE);
 
         verify(salarySlipPrinter).printLine("Employee ID: 12345");
     }
 
     @Test
-    public void print_employeID_56789_for_an_employee_with_ID_56789() {
-        salarySlipGenerator.generateFor(EMPLOYEE_56789_PAUL);
-
-        verify(salarySlipPrinter).printLine("Employee ID: 56789");
-    }
-
-    @Test
-    public void print_employeName_John_for_an_employee_with_name_John() {
-        salarySlipGenerator.generateFor(EMPLOYEE_12345_JOHN);
+    public void print_employeName_for_an_employee() {
+        salarySlipGenerator.generateFor(EMPLOYEE);
 
         verify(salarySlipPrinter).printLine("Employee Name: John J Doe");
-    }
-
-    @Test
-    public void print_employeName_Paul_for_an_employee_with_name_Paul() {
-        salarySlipGenerator.generateFor(EMPLOYEE_56789_PAUL);
-
-        verify(salarySlipPrinter).printLine("Employee Name: Paul J Doe");
     }
 }
