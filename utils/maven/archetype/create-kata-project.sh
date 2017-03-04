de2e04f9c4c50a466885c1f1e730451a69ffc2e3
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 13:54:31 +0100

    Bye bye Eclipse

--- utils/maven/archetype/create-kata-project.sh
+++ utils/maven/archetype/create-kata-project.sh
@@ -1,4 +1,1 @@
-mvn archetype:generate -DarchetypeGroupId=es.rachelcarmena -DarchetypeArtifactId=code-kata -DarchetypeVersion=1.0 -DgroupId=es.rachelcarmena -DartifactId=example
-cd example
-mvn eclipse:eclipse
-cd ..
+mvn archetype:generate -DarchetypeGroupId=es.rachelcarmena -DarchetypeArtifactId=code-kata -DarchetypeVersion=1.0 -DgroupId=es.rachelcarmena
