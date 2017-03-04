3231d48b880ff78a572b75850a95262cd5e6cdac
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 17:27:08 +0100

    Refactoring: tests organization

--- katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/SalarySlipGeneratorShould.java
+++ katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/SalarySlipGeneratorShould.java
@@ -4,7 +4,7 @@ import es.rachelcarmena.AnnualGrossSalary;
 import es.rachelcarmena.Employee;
 import es.rachelcarmena.SalarySlipGenerator;
 import es.rachelcarmena.SalarySlipPrinter;
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -17,24 +17,27 @@ public class SalarySlipGeneratorShould {
 
     @Mock SalarySlipPrinter salarySlipPrinter;
 
+    private static final AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(24000);
+    private static final Employee EMPLOYEE_12345_JOHN = new Employee(12345, "John J Doe", annualGrossSalary);
+    private static final Employee EMPLOYEE_56789_PAUL = new Employee(56789, "Paul J Doe", annualGrossSalary);
+
+    private SalarySlipGenerator salarySlipGenerator;
+
+    @Before
+    public void initialize() {
+        salarySlipGenerator = new SalarySlipGenerator(salarySlipPrinter);
+    }
+
     @Test
     public void print_employeID_12345_for_an_employee_with_ID_12345() {
-        AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(24000);
-        Employee employee = new Employee(12345, "John J Doe", annualGrossSalary);
-
-        SalarySlipGenerator salarySlipGenerator = new SalarySlipGenerator(salarySlipPrinter);
-        salarySlipGenerator.generateFor(employee);
+        salarySlipGenerator.generateFor(EMPLOYEE_12345_JOHN);
 
         verify(salarySlipPrinter).printLine("Employee ID: 12345");
     }
 
     @Test
     public void print_employeID_56789_for_an_employee_with_ID_56789() {
-        AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(24000);
-        Employee employee = new Employee(56789, "John J Doe", annualGrossSalary);
-
-        SalarySlipGenerator salarySlipGenerator = new SalarySlipGenerator(salarySlipPrinter);
-        salarySlipGenerator.generateFor(employee);
+        salarySlipGenerator.generateFor(EMPLOYEE_56789_PAUL);
 
         verify(salarySlipPrinter).printLine("Employee ID: 56789");
     }
