8a0dcb00071bccb948e1c62be18a38ebf4aa9014
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-04 17:05:45 +0100

    Red unit test

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
@@ -1,9 +1,14 @@
 package es.rachelcarmena;
 
 public class SalarySlipGenerator {
+    private SalarySlipPrinter salarySlipPrinter;
+
     public SalarySlipGenerator(SalarySlipPrinter salarySlipPrinter) {
+        this.salarySlipPrinter = salarySlipPrinter;
     }
 
     public void generateFor(Employee employee) {
+        salarySlipPrinter.printLine("Employee ID: 12345");
+
     }
 }
