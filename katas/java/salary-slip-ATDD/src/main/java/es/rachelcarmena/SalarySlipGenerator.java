b2dff28ea0462a49983606d1e1b0a2159418c557
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-05 14:51:21 +0100

    Refactoring: replacing the use of 'header' by 'description'

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/SalarySlipGenerator.java
@@ -21,41 +21,41 @@ public class SalarySlipGenerator {
     }
 
     public void generateFor(Employee employee) {
-        final String HEADER_EMPLOYEE_ID = "Employee ID";
-        final String HEADER_EMPLOYEE_NAME = "Employee Name";
-        final String HEADER_EMPLOYEE_MONTHLY_GROSS_SALARY = "Gross Salary";
-        final String HEADER_NATIONAL_INSURANCE_CONTRIBUTION = "National Insurance contributions";
-        final String HEADER_TAX_FREE_ALLOWANCE = "Tax-free allowance";
-        final String HEADER_TAXABLE_INCOME = "Taxable income";
+        final String DESCRIPTION_EMPLOYEE_ID = "Employee ID";
+        final String DESCRIPTION_EMPLOYEE_NAME = "Employee Name";
+        final String DESCRIPTION_EMPLOYEE_MONTHLY_GROSS_SALARY = "Gross Salary";
+        final String DESCRIPTION_NATIONAL_INSURANCE_CONTRIBUTION = "National Insurance contributions";
+        final String DESCRIPTION_TAX_FREE_ALLOWANCE = "Tax-free allowance";
+        final String DESCRIPTION_TAXABLE_INCOME = "Taxable income";
 
-        String line = formatPrintedLine(HEADER_EMPLOYEE_ID, String.valueOf(employee.employeeID));
+        String line = formatPrintedLine(DESCRIPTION_EMPLOYEE_ID, String.valueOf(employee.employeeID));
         console.printLine(line);
 
-        line = formatPrintedLine(HEADER_EMPLOYEE_NAME, employee.employeeName);
+        line = formatPrintedLine(DESCRIPTION_EMPLOYEE_NAME, employee.employeeName);
         console.printLine(line);
 
         MonthlyGrossSalary monthlyGrossSalary = monthlyGrossSalaryCalculator.calculate(employee.annualGrossSalary);
-        line = formatPrintedLine(HEADER_EMPLOYEE_MONTHLY_GROSS_SALARY, monthlyGrossSalary);
+        line = formatPrintedLine(DESCRIPTION_EMPLOYEE_MONTHLY_GROSS_SALARY, monthlyGrossSalary);
         console.printLine(line);
 
         Amount nationalInsuranceContribution = nationalInsuranceContributionCalculator.calculate(employee.annualGrossSalary);
-        line = formatPrintedLine(HEADER_NATIONAL_INSURANCE_CONTRIBUTION, nationalInsuranceContribution);
+        line = formatPrintedLine(DESCRIPTION_NATIONAL_INSURANCE_CONTRIBUTION, nationalInsuranceContribution);
         console.printLine(line);
 
         Amount taxFreeAllowance = taxesCalculator.calculateFreeAllowance(monthlyGrossSalary);
-        line = formatPrintedLine(HEADER_TAX_FREE_ALLOWANCE, taxFreeAllowance);
+        line = formatPrintedLine(DESCRIPTION_TAX_FREE_ALLOWANCE, taxFreeAllowance);
         console.printLine(line);
 
         Amount taxableIncome = taxesCalculator.calculateTaxableIncome(monthlyGrossSalary, taxFreeAllowance);
-        line = formatPrintedLine(HEADER_TAXABLE_INCOME, taxableIncome);
+        line = formatPrintedLine(DESCRIPTION_TAXABLE_INCOME, taxableIncome);
         console.printLine(line);
     }
 
-    private String formatPrintedLine(String header, String value) {
-        return String.format("%s: %s", header, value);
+    private String formatPrintedLine(String description, String value) {
+        return String.format("%s: %s", description, value);
     }
 
-    private String formatPrintedLine(String header, Amount amount) {
-        return formatPrintedLine(header, "£" + amount.toString());
+    private String formatPrintedLine(String description, Amount amount) {
+        return formatPrintedLine(description, "£" + amount.toString());
     }
 }
