b6c6ae7077e307717c93bde2dbbe424aaa1c0cf5
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-05 14:29:53 +0100

    Fixing mutability

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/Employee.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/Employee.java
@@ -4,9 +4,9 @@ import es.rachelcarmena.model.AnnualGrossSalary;
 
 public class Employee {
 
-    protected int employeeID;
-    protected String employeeName;
-    protected AnnualGrossSalary annualGrossSalary;
+    protected final int employeeID;
+    protected final String employeeName;
+    protected final AnnualGrossSalary annualGrossSalary;
 
     public Employee(int employeeID, String employeeName, AnnualGrossSalary annualGrossSalary) {
         this.employeeID = employeeID;
