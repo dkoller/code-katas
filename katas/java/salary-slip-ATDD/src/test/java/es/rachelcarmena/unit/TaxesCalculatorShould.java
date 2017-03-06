489e53bd9d15ee1287f3766fb3825b8d8b200d9b
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-06 17:26:26 +0100

    Red: calculate tax payable for higher rate band (past of 100K)

--- katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/TaxesCalculatorShould.java
+++ katas/java/salary-slip-ATDD/src/test/java/es/rachelcarmena/unit/TaxesCalculatorShould.java
@@ -81,7 +81,7 @@ public class TaxesCalculatorShould {
     }
 
     @Test
-    @Parameters({"45000, 7200", "50000, 9200", "60000, 13200"})
+    @Parameters({"45000, 7200", "50000, 9200", "60000, 13200", "100000, 29200", "105500, 32500"})
     public void calculate_tax_payable_for_higher_rate_band(int annualGrossSalaryAmount, int taxPayable) {
         AnnualGrossSalary annualGrossSalary = new AnnualGrossSalary(annualGrossSalaryAmount);
 
