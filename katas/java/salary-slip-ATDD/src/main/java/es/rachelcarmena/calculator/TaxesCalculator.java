7d379c52e8870f7951a5fef506e2433096590550
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-06 16:51:02 +0100

    Green: calculate tax payable for basic rate band

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/calculator/TaxesCalculator.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/calculator/TaxesCalculator.java
@@ -18,6 +18,13 @@ public class TaxesCalculator {
     }
 
     public Amount calculateTaxPayable(AnnualGrossSalary annualGrossSalary) {
+        final Amount MAX_LIMIT_BASIC_RATE = new Amount(11000);
+
+        Amount basicExcess = annualGrossSalary.subtract(MAX_LIMIT_BASIC_RATE);
+        boolean hasBasicExcess = basicExcess.greaterThanZero();
+        if (hasBasicExcess) {
+            return basicExcess.calculatePercentage(20);
+        }
         return new Amount(0);
     }
 }
