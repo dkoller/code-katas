86717ea5d75dde92fb4206aff65e4588c0c86a15
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-06 22:43:01 +0100

    Refactoring: applying the same iteration for national insurance contributions

--- katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/calculator/NationalInsuranceContributionCalculator.java
+++ katas/java/salary-slip-ATDD/src/main/java/es/rachelcarmena/calculator/NationalInsuranceContributionCalculator.java
@@ -3,27 +3,37 @@ package es.rachelcarmena.calculator;
 import es.rachelcarmena.model.Amount;
 import es.rachelcarmena.model.AnnualGrossSalary;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NationalInsuranceContributionCalculator {
 
     public Amount calculate(AnnualGrossSalary annualGrossSalary) {
+
+        Amount nationalInsuranceContributions = new Amount(0);
+        Amount salaryUnderTax = annualGrossSalary;
+
+        List<LimitAndRateRelation> limitAndRateRelations = getLimitAndRateRelations();
+        for (LimitAndRateRelation limitAndRateRelation: limitAndRateRelations) {
+            Amount amount = salaryUnderTax.subtract(limitAndRateRelation.limit);
+            boolean hasExcess = amount.greaterThanZero();
+            if (hasExcess) {
+                nationalInsuranceContributions = nationalInsuranceContributions.add(amount.calculatePercentage(limitAndRateRelation.rate));
+                salaryUnderTax = limitAndRateRelation.limit;
+            }
+        }
+        return nationalInsuranceContributions.perMonth();
+    }
+
+    private List<LimitAndRateRelation> getLimitAndRateRelations() {
         final Amount MAX_LIMIT_NO_CONTRIBUTIONS = new Amount("8060.00");
         final Amount MAX_LIMIT_BASIC_CONTRIBUTIONS = new Amount("43000.00");
         final byte BASIC_RATE = 12;
         final byte HIGHER_RATE = 2;
 
-        Amount basicExcess = annualGrossSalary.subtract(MAX_LIMIT_NO_CONTRIBUTIONS);
-        boolean hasBasicExcess = basicExcess.greaterThanZero();
-        if (!hasBasicExcess)
-            return new Amount("0.00");
-
-        Amount higherExcess = annualGrossSalary.subtract(MAX_LIMIT_BASIC_CONTRIBUTIONS);
-        boolean hasHigherExcess = higherExcess.greaterThanZero();
-        if (!hasHigherExcess)
-            return basicExcess.calculatePercentage(BASIC_RATE).perMonth();
-
-        Amount rangeBasicContributions = MAX_LIMIT_BASIC_CONTRIBUTIONS.subtract(MAX_LIMIT_NO_CONTRIBUTIONS);
-        Amount contributions = rangeBasicContributions.calculatePercentage(BASIC_RATE);
-        contributions = contributions.add(higherExcess.calculatePercentage(HIGHER_RATE));
-        return contributions.perMonth();
+        List<LimitAndRateRelation> limitAndRateRelations = new ArrayList<>();
+        limitAndRateRelations.add(new LimitAndRateRelation(MAX_LIMIT_BASIC_CONTRIBUTIONS, HIGHER_RATE));
+        limitAndRateRelations.add(new LimitAndRateRelation(MAX_LIMIT_NO_CONTRIBUTIONS, BASIC_RATE));
+        return limitAndRateRelations;
     }
 }
