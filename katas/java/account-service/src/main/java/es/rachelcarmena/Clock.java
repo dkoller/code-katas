7e33c49a1b8dfdd35e535ff34b58211400bacbf3
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-12 14:45:32 +0000

    Clock implementation

--- katas/java/account-service/src/main/java/es/rachelcarmena/Clock.java
+++ katas/java/account-service/src/main/java/es/rachelcarmena/Clock.java
@@ -1,7 +1,14 @@
 package es.rachelcarmena;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Clock {
+
     public String now() {
-        return "";
+        final String DATE_FORMAT = "dd/MM/yyyy";
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+        return LocalDate.now().format(dateTimeFormatter);
     }
 }
