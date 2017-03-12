83c90250f9ad9b23c75438e944a11a133a36c6ce
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-12 13:35:48 +0000

    Refactoring: removing duplicated code in tests

--- katas/java/account-service/src/test/java/es/rachelcarmena/unit/AccountServiceShould.java
+++ katas/java/account-service/src/test/java/es/rachelcarmena/unit/AccountServiceShould.java
@@ -1,6 +1,7 @@
 package es.rachelcarmena.unit;
 
 import es.rachelcarmena.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -11,17 +12,23 @@ import static org.mockito.Mockito.verify;
 @RunWith(MockitoJUnitRunner.class)
 public class AccountServiceShould {
 
+    private static final int ANY_AMOUNT = 200;
+
+    public AccountService accountService;
+
     @Mock
     TransactionRepository transactionRepository;
 
-    @Test
-    public void add_a_deposit_in_transaction_repository_when_deposit() {
-        final int ANY_AMOUNT = 200;
+    @Before
+    public void setUp() {
         Clock clock = new Clock();
         Console console = new Console();
         StatementPrinter statementPrinter = new StatementPrinter(console);
-        AccountService accountService = new AccountService(transactionRepository, clock, statementPrinter);
+        accountService = new AccountService(transactionRepository, clock, statementPrinter);
+    }
 
+    @Test
+    public void add_a_deposit_in_transaction_repository_when_deposit() {
         accountService.deposit(ANY_AMOUNT);
 
         verify(transactionRepository).addDeposit(ANY_AMOUNT);
@@ -29,12 +36,6 @@ public class AccountServiceShould {
 
     @Test
     public void add_a_withdraw_in_transaction_repository_when_withdraw() {
-        final int ANY_AMOUNT = 200;
-        Clock clock = new Clock();
-        Console console = new Console();
-        StatementPrinter statementPrinter = new StatementPrinter(console);
-        AccountService accountService = new AccountService(transactionRepository, clock, statementPrinter);
-
         accountService.withdraw(ANY_AMOUNT);
 
         verify(transactionRepository).addWithdraw(ANY_AMOUNT);
