1275058961f2a90141910ed0a0066f45d52c2cd8
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-12 15:25:41 +0000

    Green unit test: transaction repository should create and store a deposit when add a deposit

--- katas/java/account-service/src/main/java/es/rachelcarmena/TransactionRepository.java
+++ katas/java/account-service/src/main/java/es/rachelcarmena/TransactionRepository.java
@@ -1,21 +1,26 @@
 package es.rachelcarmena;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionRepository {
     private Clock clock;
+    private List<Transaction> transactionList;
 
     public TransactionRepository(Clock clock) {
         this.clock = clock;
+        this.transactionList = new ArrayList<>();
     }
 
     public void addDeposit(int amount) {
+        Transaction deposit = new Deposit(amount, clock.now());
+        transactionList.add(deposit);
     }
 
     public void addWithdraw(int amount) {
     }
 
     public List<Transaction> allTransactions() {
-        return null;
+        return transactionList;
     }
 }
