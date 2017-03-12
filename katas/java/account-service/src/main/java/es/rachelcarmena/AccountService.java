86e219d904857ae28b7c234fc55f3e49837af6f6
Rachel <raquel.moreno.carmena@gmail.com>
2017-03-12 13:31:38 +0000

    Green unit test: add a deposit in transaction repository when deposit

--- katas/java/account-service/src/main/java/es/rachelcarmena/AccountService.java
+++ katas/java/account-service/src/main/java/es/rachelcarmena/AccountService.java
@@ -1,10 +1,18 @@
 package es.rachelcarmena;
 
 public class AccountService {
+    private final TransactionRepository transactionRepository;
+    private final Clock clock;
+    private final StatementPrinter statementPrinter;
+
     public AccountService(TransactionRepository transactionRepository, Clock clock, StatementPrinter statementPrinter) {
+        this.transactionRepository = transactionRepository;
+        this.clock = clock;
+        this.statementPrinter = statementPrinter;
     }
 
     public void deposit(int amount) {
+        transactionRepository.addDeposit(amount);
     }
 
     public void withdraw(int amount) {
