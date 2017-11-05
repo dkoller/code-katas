6b8664c3ab24a58d10d9e65cbc4b21b342ae11cd
Rachel <raquel.moreno.carmena@gmail.com>
2017-11-05 20:00:05 +0100

    Having more than 2 names

--- katas/java/greeting/src/main/java/es/rachelcarmena/Greeting.java
+++ katas/java/greeting/src/main/java/es/rachelcarmena/Greeting.java
@@ -7,6 +7,12 @@ public class Greeting {
         if (names.length == 2) {
             return String.format("Hello, %s and %s.", names[0], names[1]);
         }
+        if (names.length > 2) {
+            String previousNames = "";
+            for (int index = 0; index < names.length - 1; index++)
+                previousNames += String.format(" %s,", names[index]);
+            return String.format("Hello,%s and %s.", previousNames, names[names.length - 1]);
+        }
         if (names[0].toUpperCase().equals(names[0]))
             return String.format("HELLO %s!", names[0]);
         return String.format("Hello, %s.", names[0]);
--- katas/java/greeting/src/test/java/es/rachelcarmena/GreetingShould.java
+++ katas/java/greeting/src/test/java/es/rachelcarmena/GreetingShould.java
@@ -34,4 +34,9 @@ public class GreetingShould {
 	public void create_a_multiple_greeting_when_two_names() {
 		assertThat(greeting.greet("Jill", "Jane"), is("Hello, Jill and Jane."));
 	}
+
+	@Test
+	public void create_a_multiple_greeting_when_more_than_two_names() {
+		assertThat(greeting.greet("Amy", "Brian", "Charlotte"), is("Hello, Amy, Brian, and Charlotte."));
+	}
 }
