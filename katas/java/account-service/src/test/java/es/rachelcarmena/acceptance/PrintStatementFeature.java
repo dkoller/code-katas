7bcf819fc30752b5882740acaee6e00c470c430e
Rachel <raquel.moreno.carmena@gmail.com>
2018-02-06 20:18:06 +0200

    Fixing order in verifications

--- katas/java/account-service/src/test/java/es/rachelcarmena/acceptance/PrintStatementFeature.java
+++ katas/java/account-service/src/test/java/es/rachelcarmena/acceptance/PrintStatementFeature.java
@@ -8,7 +8,9 @@ import es.rachelcarmena.repository.TransactionRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.LocalDate;
@@ -24,28 +26,31 @@ public class PrintStatementFeature {
     @Mock
     Clock clock;
     private AccountService accountService;
+    private InOrder consoleInOrder;
 
     @Before
     public void setUp() {
         StatementPrinter statementPrinter = new StatementPrinter(console);
         TransactionRepository transactionRepository = new TransactionRepository();
         accountService = new AccountService(transactionRepository, statementPrinter, clock);
+        consoleInOrder = Mockito.inOrder(console);
     }
 
     @Test
     public void should_print_a_bank_statement_in_reversed_chronological_order() {
-        given(clock.now()).willReturn(LocalDate.of(2014, 4, 1));
+        given(clock.now()).willReturn(
+                LocalDate.of(2014, 4, 1),
+                LocalDate.of(2014, 4, 2),
+                LocalDate.of(2014, 4, 10));
         accountService.deposit(1000);
-        given(clock.now()).willReturn(LocalDate.of(2014, 4, 2));
         accountService.withdraw(100);
-        given(clock.now()).willReturn(LocalDate.of(2014, 4, 10));
         accountService.deposit(500);
 
         accountService.printStatement();
 
-        verify(console).print("DATE       | AMOUNT  | BALANCE");
-        verify(console).print("10/04/2014 | 500.00  | 1400.00");
-        verify(console).print("02/04/2014 | -100.00 | 900.00 ");
-        verify(console).print("01/04/2014 | 1000.00 | 1000.00");
+        consoleInOrder.verify(console).print("DATE       | AMOUNT  | BALANCE");
+        consoleInOrder.verify(console).print("10/04/2014 | 500.00  | 1400.00");
+        consoleInOrder.verify(console).print("02/04/2014 | -100.00 | 900.00 ");
+        consoleInOrder.verify(console).print("01/04/2014 | 1000.00 | 1000.00");
     }
 }
